7549652b9b64211fc8ebfae03550d3aa65ab8325
HJC96 <gkswlcjs2@naver.com>
2025-04-13 11:50:08 +0900

    FEAT: CertificationService 테스트 추가

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import com.example.demo.user.infrastructure.MailSenderImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CertificationServiceTest {
+
+    @Test
+    public void 이메일과_컨테츠가_제대로_만들어져서_보내지는지_테스트한다(){
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send("gkswlcjs2@naver.com", 1, "aaaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo("gkswlcjs2@naver.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+
+    }
+}
