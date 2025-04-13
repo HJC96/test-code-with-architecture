bc10dfc9e009038024f4f7971cbe06ec4d4f2da8
HJC96 <gkswlcjs2@naver.com>
2025-04-13 17:26:08 +0900

    FEAT: 테스트 코드 보강 및 Clock, Uuid 처리

--- src/test/java/com/example/demo/post/controller/response/PostResponseTest.java
+++ src/test/java/com/example/demo/post/controller/response/PostResponseTest.java
@@ -1,13 +1,35 @@
 package com.example.demo.post.controller.response;
 
+import com.example.demo.post.domain.Post;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PostResponseTest {
 
     @Test
-    public void User으로_응답을_생성할_수_있다(){
+    public void Post으로_응답을_생성할_수_있다(){
         // given
+        Post post = Post.builder()
+                .content("helloworld")
+                .writer(User.builder()
+                        .email("gkswlcjs2@naver.com")
+                        .nickname("gkswlcjs2")
+                        .address("서울시 강남구")
+                        .status(UserStatus.ACTIVE)
+                        .certificationCode("aaaaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+                        .build()
+                )
+                .build();
         // when
+        PostResponse postResponse = PostResponse.from(post);
+
         // then
+        assertThat(postResponse.getContent()).isEqualTo("helloworld");
+        assertThat(postResponse.getWriter().getEmail()).isEqualTo("gkswlcjs2@naver.com");
+        assertThat(postResponse.getWriter().getNickname()).isEqualTo("gkswlcjs2");
+        assertThat(postResponse.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
     }
 }
